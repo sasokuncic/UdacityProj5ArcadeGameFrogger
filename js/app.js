4952b276799b298edab2422258dd6924189f7af8
sasokuncic <saso.kuncic@gmail.com>
2018-08-12 21:45:25 +0200

    Resolved error messages in the Web Development Tools. Opened missing items.

--- js/app.js
+++ js/app.js
@@ -21,16 +21,36 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player who have to avoid enemies and jump into the water
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+};
 
+// Update the player's position according to pressed arrow keys
+Player.prototype.update = function() {
+    // TODO check for the collison
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Change the player's position according to pressed arrow keys 
+Player.prototype.handleInput = function(keyPressed) {
+    // moving logic
+};
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+
+// Place all enemy objectsvar allEnemies = []; in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = [];
 
-
+// Instantiate the player's object.
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
